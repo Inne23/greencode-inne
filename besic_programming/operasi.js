7e26062c1aab7dbbc638db2e1d375dc16c9586af
Inne23 <innefebryanda@gmail.com>
2023-10-02 17:49:58 +0700

    add operator

---
+++ besic_programming/operasi.js
@@ -0,0 +1,26 @@
+let angka = 5 + 5 + 10
+
+
+let angka1 = 12
+let angka2 = 7
+let pertambahan = angka1 + angka2
+
+let firstname = "inne"
+let lastname = "febryanda"
+let fullname = firstname +" " + lastname
+
+let angka4 = 7
+let angka5 = 4
+let pengurangan = angka4 - angka5
+let perkalian = angka4 * angka5
+let pembagian = angka4 / angka5
+let sisabagi = angka4 % angka5
+
+
+
+
+console.log(pertambahan)
+console.log(pengurangan)
+console.log(perkalian)
+console.log(pembagian)
+console.log(sisabagi)
---
+++ besic_programming/variabel.js
@@ -0,0 +1,14 @@
+var mynime = "inne"
+let myAge = "20"
+const myAdresss = "Silangkitang"
+let ismahasiswa = true
+let makanan 
+let minuman = null
+
+console.log (mynime)
+console.log (myAge)
+console.log (myAdresss)
+console.log (ismahasiswa)
+console.log (makanan)
+console.log (minuman)
+
