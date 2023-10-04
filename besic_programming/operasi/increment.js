ea8bd1388533a8b461558948e5dfb96cb7984c61
Inne23 <innefebryanda@gmail.com>
2023-10-04 15:10:15 +0700

    add.perbandingan

---
+++ besic_programming/operasi/increment.js
@@ -0,0 +1,12 @@
+let z = 20
+z+=3
+z-=3
+z*=3
+z/=3
+z%=3
+
+
+let bolls = true+true
+
+
+console.log(bolls)
---
+++ besic_programming/operasi/perbandingan.js
@@ -0,0 +1,15 @@
+let lebihbesar = 6>5
+let lebihkecil= 5<7
+let lebihkecilsamadengan= 5<=8
+let lebihbesarsamadengan= 8>=5
+let samadengan= 3 ==5
+let tidaksamadengan= 9 != 7
+let samadengansamadengan="6"==6
+
+console.log(lebihbesar)
+console.log(lebihkecil)
+console.log(lebihkecilsamadengan)
+console.log(lebihbesarsamadengan)
+console.log(samadengan)
+console.log(tidaksamadengan)
+console.log(samadengansamadengan)
