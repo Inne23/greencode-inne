eba3eab546435c59517c5ce329e8339d687f24fc
Inne23 <innefebryanda@gmail.com>
2023-10-09 17:37:00 +0700

    add perulangan

---
+++ besic_programming/perulangan/perulangan.js
@@ -0,0 +1,21 @@
+for (let index = 0; index <=100; index++) {
+    
+    
+}
+
+for (let index = 1 ; index <= 100; index += 2) {
+   
+    
+}
+// ini perulangan
+let fruits = ["mangga", "rambutan", "jambu"]
+for (let index = 0; index < 3; index++) {
+  
+}
+
+let fruits1 = ["mangga","rambutan", "jambu"]
+for (let index = 0; index < fruits.length; index++) {
+    console.log(fruits[index])
+}
+    
+    
