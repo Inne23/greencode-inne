1b36a775b3421598532c24bc8c9506e51990ddf8
Inne23 <innefebryanda@gmail.com>
2023-10-09 16:41:39 +0700

    addperkondisian

---
+++ besic_programming/perkondisian/perkondisian.js
@@ -0,0 +1,37 @@
+//ini perkondisian
+let mahasiswa = false
+
+if (mahasiswa == true){
+    console.log("aku mahasiswa")
+    
+}else{
+    console.log("aku bukan mahasiswa")
+
+}
+
+// ini perkondisian
+let nilai = 3
+let isMahasiswa = true
+
+if (isMahasiswa == true) {
+    nilai ++
+}else{
+    nilai --
+}
+console.log(nilai)
+
+// ini pekondisian
+let angka = 75
+let hasil = "nilaiku"
+
+if (angka> 90) {
+    hasil="nilai bagus"
+    
+}else if (angka <80 && angka> 70) {
+    hasil="nilai cukup bagus"
+    
+}else if(angka < 70) {
+    hasil="nilai buruk"
+    
+}
+console.log(hasil)
